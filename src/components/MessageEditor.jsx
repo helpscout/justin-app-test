162c576ba920a84fc5c6230a0a9aeaf6c29944b1
justin wolfe <justin@helpscout.com>
2023-06-18 08:50:56 -0700

    start styling editor

--- src/components/MessageEditor.jsx
+++ src/components/MessageEditor.jsx
@@ -1,9 +1,20 @@
+import styled from 'styled-components';
+
+const TextareaUI = styled.textarea`
+  height: 244px;
+  margin-bottom: 10px;
+  width: 225px;
+  padding: 1em;
+  line-height: 1.5em;
+  background-color: white;
+`;
+
 const MessageEditor = ({ text, setText }) => {
   const handleChange = (event) => {
     setText(event.target.value);
   };
 
-  return <textarea value={text} onChange={handleChange} />;
+  return <TextareaUI value={text} onChange={handleChange} />;
 };
 
 export default MessageEditor;
