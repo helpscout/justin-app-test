5c6c34d09de8d50d40cf1376fce37b3fe967c087
justin wolfe <justin@helpscout.com>
2023-06-18 17:14:09 -0700

    animation

--- src/components/ConversationPanel.jsx
+++ src/components/ConversationPanel.jsx
@@ -1,11 +1,12 @@
+import { useSpring, animated, useTransition } from 'react-spring';
 import MessageEditor from './MessageEditor';
 import { Button, Heading } from '@helpscout/ui-kit';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createSlackThread } from '../utils/api';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import MessagesList from './MessagesList.jsx';
 
-const ButtonWrapper = styled.div`
+const AnimatedButtonWrapper = styled(animated.div)`
   display: flex;
   align-items: flex-end;
   justify-content: flex-end;
@@ -13,36 +14,78 @@ const ButtonWrapper = styled.div`
   margin-bottom: 10px;
 `;
 
+const AnimatedMessageEditor = styled(animated.div)``;
+
 const ConversationPanelWrapper = styled.div`
   overflow: scroll;
   height: 300px;
   padding-right: 8px;
 `;
 
-const Composer = ({ text, setText, onSendMessage, hasMessages }) => (
-  <>
-    <MessageEditor text={text} setText={setText} />
-    <ButtonWrapper hasMessages={hasMessages}>
-      <Button size="sm" onClick={onSendMessage}>
-        Send to Slack
-      </Button>
-    </ButtonWrapper>
-  </>
-);
+const Composer = ({ text, setText, onSendMessage, hasMessages }) => {
+  const [startAnimation, setStartAnimation] = useState(false);
+
+  const buttonAnimation = useSpring({
+    opacity: startAnimation ? 0 : 1,
+    delay: 200,
+    onRest: startAnimation ? onSendMessage : null,
+  });
+
+  const textAreaAnimation = useSpring({
+    opacity: startAnimation ? 0 : 1,
+    delay: 400,
+  });
+
+  const handleClick = () => {
+    setStartAnimation(true);
+  };
+
+  return (
+    <>
+      <AnimatedMessageEditor style={textAreaAnimation}>
+        <MessageEditor text={text} setText={setText} />
+      </AnimatedMessageEditor>
+      <AnimatedButtonWrapper style={buttonAnimation} hasMessages={hasMessages}>
+        <Button size="sm" onClick={handleClick}>
+          Send to Slack
+        </Button>
+      </AnimatedButtonWrapper>
+    </>
+  );
+};
 
 const ConversationPanel = ({ state }) => {
   const [text, setText] = useState('');
   const { messages } = state;
+  const [displayMessages, setDisplayMessages] = useState(messages);
+  const [startListAnimation, setStartListAnimation] = useState(false);
+
+  useEffect(() => {
+    setDisplayMessages(messages);
+    if (messages.length > 0) {
+      setStartListAnimation(true);
+    }
+  }, [messages]);
+
+  const messagesAnimation = useSpring({
+    opacity: startListAnimation ? 1 : 0,
+    delay: 600,
+  });
 
   const onSendMessage = async () => {
     const conversationId = state?.conversation?.id;
     const user = state?.user;
     await createSlackThread({ text, conversationId, user });
+    setText(''); // Clear text area after sending message
   };
 
   return (
     <ConversationPanelWrapper>
-      {messages.length ? <MessagesList messages={messages} /> : null}
+      <animated.div style={messagesAnimation}>
+        {displayMessages.length > 0 ? (
+          <MessagesList messages={displayMessages} />
+        ) : null}
+      </animated.div>
       <Composer
         text={text}
         setText={setText}
